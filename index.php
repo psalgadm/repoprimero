61c75e5938686003169bd847e091b765457e03be
Pablo Salgado <psalgadm2@gmail.com>
2020-03-28 21:55:30 -0300

    segundo commit rama master

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
-echo "<pre>"; print_r("Mi primera linea"); echo "</pre>";
+echo "<pre>"; print_r("Mi primera linea master"); echo "</pre>";
 
-?>
+echo "<pre>"; print_r("Mi segunda linea master"); echo "</pre>";
